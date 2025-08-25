2c0f89d0f2af4f450aa7d77437493a24b1b82b9f
CrimsonAmir <ytchanneltinos@gmail.com>
2025-08-25 21:40:08 +0330

    add req.txt

--- tasks.py
+++ tasks.py
@@ -393,8 +393,9 @@ def check_the_service(bill_id):
 
                     update_valid_until(session, bill_id, valid_until)
                 else:
-                    fallback = (datetime.now(ZoneInfo("Asia/Tehran")) + timedelta(days=1)).astimezone(timezone.utc)
-                    update_valid_until(session, bill_id, fallback)
+                    valid_until = (datetime.now(ZoneInfo("Asia/Tehran")) + timedelta(days=1)).astimezone(timezone.utc)
+                    update_valid_until(session, bill_id, valid_until)
+
                 msg = text.get("outage_report", "outage_report").format(bill_id)
                 msg += "\n\n" + format_outages([next_outage])  # show only the next one
                 for user in users:
